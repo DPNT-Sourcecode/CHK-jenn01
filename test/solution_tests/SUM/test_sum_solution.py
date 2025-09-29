f455ffb5967df7e20e1aa21710570944b9937d0b
sbx_user1051 <sbx_user1051@169.254.2.213>
2025-09-29 20:22:40 +0000

    Mon Sep 29 20:22:40 UTC 2025

--- test/solution_tests/SUM/test_sum_solution.py
+++ test/solution_tests/SUM/test_sum_solution.py
@@ -6,10 +6,7 @@ class TestSum:
     def test_sum(self):
         assert SumSolution().compute(1, 2) == 3
 
-    def test_sum2(self):
+    def test_sum_out_of_range(self):
         with pytest.raises(ValueError) as value_exception:
-            SumSolution().compute(1, 2)
+            SumSolution().compute(1, 'a')
         assert str(value_exception.value) == "Argument should be between 0 and 100"
-
-
-
