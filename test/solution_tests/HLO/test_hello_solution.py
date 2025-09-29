093c72382c04f161717db08ad26d986d224b0ec6
sbx_user1051 <sbx_user1051@169.254.2.213>
2025-09-29 20:43:15 +0000

    Mon Sep 29 20:43:15 UTC 2025

--- test/solution_tests/HLO/test_hello_solution.py
+++ test/solution_tests/HLO/test_hello_solution.py
@@ -1,5 +1,16 @@
 import pytest
 from lib.solutions.HLO.hello_solution import  HelloSolution
 
-def test_hello():
-    solution = HelloSolution()
+class TestHelloSolution:
+    def test_hello(self):
+        assert HelloSolution().hello("John") == "Hello, John!"
+
+    def test_hello_with_empty_name(self):
+        with pytest.raises(ValueError) as exception:
+            HelloSolution().hello("")
+        assert str(exception.value) == "Please provide friend name"
+
+    def test_with_empty_not_string(self):
+        with pytest.raises(ValueError) as exception:
+            HelloSolution().hello(1)
+        assert str(exception.value) == "Please give a friend name as a string"
