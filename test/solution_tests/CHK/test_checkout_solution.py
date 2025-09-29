c23de0def6736ba23f469ac0244f4243f5473c15
sbx_user1051 <sbx_user1051@169.254.2.213>
2025-09-29 22:05:49 +0000

    Mon Sep 29 22:05:49 UTC 2025

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -6,5 +6,6 @@ class TestCheckoutSolution:
         assert -1 == CheckoutSolution().checkout("")
 
     def test_checkout_solution_with_nonempty_skus(self):
-        assert CheckoutSolution().checkout("AAABACCCCCDB") == 50
+        assert 98 == CheckoutSolution().checkout("AAABACCCCCDB")
+
 
