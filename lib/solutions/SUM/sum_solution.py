0168797ed67b79e0d6dccf50cafc3a5596aae6e0
sbx_user1051 <sbx_user1051@169.254.2.213>
2025-09-29 20:30:55 +0000

    Mon Sep 29 20:30:55 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,13 +2,11 @@
 class SumSolution:
     
     def compute(self, x, y):
-        try:
-            if not isinstance(x, int) or not isinstance(y, int):
-                raise ValueError("Arguments should be integers")
-            elif 0 <= x <= 100 and 0 <= y <= 100:
-                return x + y
-            else:
-                raise ValueError("Argument should be between 0 and 100")
-        except ValueError:
+        if not isinstance(x, int) or not isinstance(y, int):
+            raise ValueError("Arguments should be integers")
+        elif 0 <= x <= 100 and 0 <= y <= 100:
+            return x + y
+        else:
             raise ValueError("Argument should be between 0 and 100")
+
 
