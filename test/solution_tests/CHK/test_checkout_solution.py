d536f6b1b0b9697be265033421fd5e90aafedd0c
sbx_user1051 <sbx_user1051@169.254.2.213>
2025-09-30 00:25:55 +0000

    Tue Sep 30 00:25:55 UTC 2025

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -8,6 +8,9 @@ class TestCheckoutSolution:
     def test_checkout_solution_with_nonempty_skus(self):
         assert 340 == CheckoutSolution().checkout("AAABACCCCCDB")
 
+    def test_checkout_solution_with_new_goods(self):
+        assert 490 == CheckoutSolution().checkout("AAABACEEECCCCDB")
+
     def test_checkout_solution_with_invalid_skus(self):
         assert 0 == CheckoutSolution().checkout("")
         assert -1 == CheckoutSolution().checkout("a")
