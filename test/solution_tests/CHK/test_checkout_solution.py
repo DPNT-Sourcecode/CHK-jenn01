930b5fdbb3882f0974d98569399f834e616117a9
sbx_user1051 <sbx_user1051@169.254.2.213>
2025-09-30 06:48:38 +0000

    Tue Sep 30 06:48:38 UTC 2025

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -6,27 +6,19 @@ from lib.solutions.CHK.checkout_solution import CheckoutSolution
 
 
 class TestCheckoutSolution(unittest.TestCase):
-    def test_checkout_solution_with_empty_skus(self):
-        assert 0 == CheckoutSolution().checkout("")
-
-    def test_chk_bbb_solution(self):
-        assert 180 == CheckoutSolution().checkout("AAAA")
-
-    def test_checkout_solution_with_nonempty_skus(self):
-        assert 340 == CheckoutSolution().checkout("AAABACCCCCDB")
 
     @parameterized.expand([
+        ("", 0),
+        ("a", -1),
+        ("ABCa", -1),
+        ("AAAA", 180),
+        ("AAAAAAAA", 380),
+        ("AAABACCCCCDB", 340),
         ("AAAABBCCCCCDEEE", 440),
         ("AAAABBCCCCCDEEEE", 495)
     ])
     def test_checkout_solution_with_new_goods(self, expected, test_input):
         self.assertEqual(expected, CheckoutSolution().checkout(test_input))
 
-    def test_checkout_solution_with_multiple_offers(self):
-        # assert 330 == CheckoutSolution().checkout("AAAAAAAA")
-        assert 380 == CheckoutSolution().checkout("AAAAAAAAA")
-
-    def test_checkout_solution_with_invalid_skus(self):
-        assert 0 == CheckoutSolution().checkout("")
-        assert -1 == CheckoutSolution().checkout("a")
-        assert -1 == CheckoutSolution().checkout("ABCa")
+if __name__ == '__main__':
+    unittest.main()
