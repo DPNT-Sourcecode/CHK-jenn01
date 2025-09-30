54a679a64f27619a22402452451f42ed532e9e0c
sbx_user1051 <sbx_user1051@169.254.20.7>
2025-09-30 08:58:50 +0000

    Tue Sep 30 08:58:50 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -41,9 +41,7 @@ GOODS['Z'] = Product('Z', 50, "")
 DISCOUNTED_PRODUCTS = defaultdict(Product)
 DISCOUNTED_PRODUCTS['BUY_MULTIPLE_PAY_LESS'] = ["A", "H", "V"]
 DISCOUNTED_PRODUCTS['BUY_MULTIPLE_GET_FREE'] = ["E", "F", "N", "R", "U"]
-DISCOUNTED_PRODUCTS['BUY_MORE_PAY_LESS'] = (list(GOODS.keys())
-    - DISCOUNTED_PRODUCTS['BUY_MULTIPLE_PAY_LESS']
-    - DISCOUNTED_PRODUCTS['BUY_MULTIPLE_GET_FREE'])
+DISCOUNTED_PRODUCTS['BUY_MORE_PAY_LESS'] = []
 
 def apply_special_offers_for_new_good(skus):
     """
@@ -178,3 +176,4 @@ class CheckoutSolution:
         else:
             total = GOODS[sku].price * len(items)
         return total
+
