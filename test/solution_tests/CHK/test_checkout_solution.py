243101b6d52af3bd5f2c2371df97efb88523a6ea
sbx_user1051 <sbx_user1051@169.254.20.7>
2025-09-30 11:38:50 +0000

    Tue Sep 30 11:38:50 UTC 2025

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -46,15 +46,16 @@ class TestCheckoutSolution(unittest.TestCase):
         # ("ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ", 1880),
         # ("AAAAAPPPPPUUUUEEBRRRQAAAHHHHHHHHHHVVVBBNNNMFFFKKQQQVVHHHHH", 1640),
         # ('A', 50),
-        # ('ASTXYZ', 115),
-        # ('AASTXYZ', 165),
-        ('AASTTXYZSTXYZ', 145),
+        ('ASTXYZ', 135),
+        ('AASTXYZ', 185),
+        ('AASTTXYZSTXYZ', 275),
     ])
     def test_checkout_solution_with_new_goods(self, test_input, expected):
         self.assertEqual(CheckoutSolution().checkout(test_input), expected)
 
 if __name__ == '__main__':
     unittest.main()
+
 
 
 
